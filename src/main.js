36bbc4d81ea0480ba260379a975b85422d71d1bf
Sergio Rodriguez <sergrodrig@gmail.com>
2021-05-12 18:38:44 -0400

    modificaciones al procfile

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
+import LogRocket from 'logrocket'
+
+LogRocket.init('nckyti/sudoers-remuneraciones')
 
 createApp(App).use(store).use(router).mount('#app')
