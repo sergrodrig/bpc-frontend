6c0f8dc994b9cd63deb117a02024df694dfe54e0
Sergio Rodriguez <sergrodrig@gmail.com>
2021-05-12 18:41:58 -0400

    modificaciones al procfile

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const path = require('path')
 const app = express()
 
 app.use(serveStatic(path.join(__dirname, 'dist')))
+
 app.all('*', function (req, res) {
   res.redirect('/')
 })
 
-app.listen(80)
+const port = process.env.PORT || 80
+app.listen(port)
