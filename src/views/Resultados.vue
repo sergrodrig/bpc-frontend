a293c7811e2ff7a0d3e4d3b546791fc0589d17ab
Sergio Rodriguez <sergrodrig@gmail.com>
2021-05-14 01:16:57 -0400

    se modifica tabla de posiciones

--- src/views/Resultados.vue
+++ src/views/Resultados.vue
@@ -13,104 +13,104 @@
         <table class="table-auto w-full text-left whitespace-no-wrap">
           <thead>
             <tr>
-              <th class="px-6 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+              <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white rounded-tl rounded-bl">
                 Pos.
               </th>
-              <th class="px-6 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+              <th class="py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
                 Equipo
               </th>
-              <th class="px-6 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+              <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
                 Puntos
               </th>
-              <th class="px-6 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+              <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
                 Rounds ganados
               </th>
-              <th class="px-6 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+              <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
                 Rounds perdidos
               </th>
-              <th class="px-6 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+              <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white rounded-tr rounded-br">
                 Ratio
               </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td class="px-6 py-3">
+              <td class="text-center py-3">
                 1
               </td>
-              <td class="px-6 py-3">
+              <td class="py-3&quot;">
                 Chichigans
               </td>
-              <td class="px-6 py-3">
+              <td class="text-center py-3 font-bold">
                 3
               </td>
-              <td class="px-6 py-3">
+              <td class="text-center py-3">
                 36
               </td>
-              <td class="px-6 py-3 text-lg text-gray-900">
+              <td class="text-center py-3">
                 10
               </td>
-              <td class="px-6 py-3 text-lg text-gray-900">
+              <td class="text-center py-3">
                 3,6
               </td>
             </tr>
             <tr>
-              <td class="border-t-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-gray-200 text-center py-3">
                 2
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-gray-200 py-3">
                 Los del Gulag
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-gray-200 text-center py-3 font-bold">
                 1
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-gray-200 text-center py-3">
                 29
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3 text-lg text-gray-900">
+              <td class="border-t-2 border-gray-200 text-center py-3">
                 20
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3 text-lg text-gray-900">
+              <td class="border-t-2 border-gray-200 text-center py-3">
                 1,45
               </td>
             </tr>
             <tr>
-              <td class="border-t-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-gray-200 text-center py-3">
                 3
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-gray-200 py-3">
                 Mancoster United
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-gray-200 text-center py-3 font-bold">
                 1
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-gray-200 text-center py-3">
                 20
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3 text-lg text-gray-900">
+              <td class="border-t-2 border-gray-200 text-center py-3">
                 29
               </td>
-              <td class="border-t-2 border-gray-200 px-6 py-3 text-lg text-gray-900">
+              <td class="border-t-2 border-gray-200 text-center py-3">
                 0,68
               </td>
             </tr>
             <tr>
-              <td class="border-t-2 border-b-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-b-2 border-gray-200 text-center py-3">
                 4
               </td>
-              <td class="border-t-2 border-b-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-b-2 border-gray-200 py-3">
                 The Wutang Lag
               </td>
-              <td class="border-t-2 border-b-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-b-2 border-gray-200 text-center py-3 font-bold">
                 0
               </td>
-              <td class="border-t-2 border-b-2 border-gray-200 px-6 py-3">
+              <td class="border-t-2 border-b-2 border-gray-200 text-center py-3">
                 10
               </td>
-              <td class="border-t-2 border-b-2 border-gray-200 px-6 py-3 text-lg text-gray-900">
+              <td class="border-t-2 border-b-2 border-gray-200 text-center py-3">
                 36
               </td>
-              <td class="border-t-2 border-b-2 border-gray-200 px-6 py-3 text-lg text-gray-900">
+              <td class="border-t-2 border-b-2 border-gray-200 text-center py-3">
                 0,27
               </td>
             </tr>
