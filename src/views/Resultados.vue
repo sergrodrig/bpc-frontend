7c275d9ebe735fdc1bc9d30ddc105176fbfaca2b
Sergio Rodriguez <sergrodrig@gmail.com>
2021-05-17 07:28:26 -0400

    ementado el skeleton loader en resultados

--- src/views/Resultados.vue
+++ src/views/Resultados.vue
@@ -1,78 +1,103 @@
 <template>
-  <section class="text-gray-600 body-font pt-12">
-    <div class="container mx-auto max-w-5xl">
-      <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="sm:text-4xl text-3xl font-medium title-font mb-4 text-gray-200 bg-black p-2">
-          Tabla de posiciones
-        </h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-          3 puntos triunfo / 1 punto empate / 0 puntos derrota
-        </p>
-      </div>
-      <div v-if="isFetching">
-        <p>cargando...</p>
-      </div>
-      <div v-if="data">
-        <div class="lg:w-3/3 w-full mx-auto overflow-auto">
-          <table class="table-auto w-full text-left whitespace-no-wrap">
-            <thead>
-              <tr>
-                <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white rounded-tl rounded-bl">
-                  Pos.
-                </th>
-                <th class="py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
-                  Equipo
-                </th>
-                <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
-                  Puntos
-                </th>
-                <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
-                  Jugados
-                </th>
-                <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
-                  Rounds+
-                </th>
-                <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
-                  Rounds-
-                </th>
-                <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white rounded-tr rounded-br">
-                  Ratio
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="value,key in data.allPuntajes"
-                :key="value.id"
-              >
-                <td class="text-center py-3">
-                  {{ key + 1 }}
-                </td>
-                <td class="py-3&quot;">
-                  {{ value.team.name }}
-                </td>
-                <td class="text-center py-3 font-bold">
-                  {{ value.points }}
-                </td>
-                <td class="text-center py-3">
-                  {{ value.matchPlayed }}
-                </td>
-                <td class="text-center py-3">
-                  {{ value.roundsWon }}
-                </td>
-                <td class="text-center py-3">
-                  {{ value.roundsLost }}
-                </td>
-                <td class="text-center py-3">
-                  {{ value.roundsWLRatio }}
-                </td>
-              </tr>
-            </tbody>
-          </table>
+  <transition
+    name="fade"
+    mode="out-in"
+  >
+    <section class="text-gray-600 body-font pt-12">
+      <div class="container mx-auto max-w-5xl">
+        <div class="flex flex-col text-center w-full mb-20">
+          <h1 class="sm:text-4xl text-3xl font-medium title-font mb-4 text-gray-200 bg-black p-2">
+            Tabla de posiciones
+          </h1>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+            3 puntos triunfo / 1 punto empate / 0 puntos derrota
+          </p>
+        </div>
+        <div v-if="isFetching">
+          <div class="lg:w-3/3 w-full mx-auto overflow-auto animate-pulse">
+            <table class="table-auto w-full text-left whitespace-no-wrap">
+              <thead class="bg-gray-400 h-12 rounded-lg">
+                <tr>
+                  <th
+                    v-for="keyd in 7"
+                    :key="keyd"
+                  />
+                </tr>
+              </thead>
+              <tbody class="space-y-4">
+                <tr>
+                  <td
+                    v-for="keyc in 7"
+                    :key="keyc"
+                    class="bg-gray-200 h-72 m-4"
+                  />
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div v-if="data">
+          <div class="lg:w-3/3 w-full mx-auto overflow-auto">
+            <table class="table-auto w-full text-left whitespace-no-wrap">
+              <thead>
+                <tr>
+                  <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white rounded-tl rounded-bl">
+                    Pos.
+                  </th>
+                  <th class="py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
+                    Equipo
+                  </th>
+                  <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
+                    Puntos
+                  </th>
+                  <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
+                    Jugados
+                  </th>
+                  <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
+                    Rounds+
+                  </th>
+                  <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white">
+                    Rounds-
+                  </th>
+                  <th class="text-center py-3 tracking-wider font-medium text-sm bg-indigo-700 text-white rounded-tr rounded-br">
+                    Ratio
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="value,key in data.allPuntajes"
+                  :key="value.id"
+                >
+                  <td class="text-center py-3">
+                    {{ key + 1 }}
+                  </td>
+                  <td class="py-3&quot;">
+                    {{ value.team.name }}
+                  </td>
+                  <td class="text-center py-3 font-bold">
+                    {{ value.points }}
+                  </td>
+                  <td class="text-center py-3">
+                    {{ value.matchPlayed }}
+                  </td>
+                  <td class="text-center py-3">
+                    {{ value.roundsWon }}
+                  </td>
+                  <td class="text-center py-3">
+                    {{ value.roundsLost }}
+                  </td>
+                  <td class="text-center py-3">
+                    {{ value.roundsWLRatio }}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </transition>
 </template>
 
 <script>
@@ -106,6 +131,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
