f2a8988aa7faf3182f6df6f67ae0be37ed612fc2
Sergio Rodriguez <sergrodrig@gmail.com>
2021-05-18 05:11:00 -0400

    agregada la data de los resultados

--- src/components/ResultadosFechas.vue
+++ src/components/ResultadosFechas.vue
@@ -12,21 +12,25 @@
           {{ fecha.fecha }}
         </h2>
       </div>
-      <div>
-        <div
-          v-for="duelo in fecha.duelos"
-          :key="duelo"
-          class="grid grid-cols-11"
-        >
-          <div class="text-right col-start-2 col-end-5">
-            {{ duelo.local }}
-          </div>
-          <div class="text-center col-start-6 col-end-7">
-            vs
-          </div>
-          <div class="text-left col-start-8 col-end-11">
-            {{ duelo.visita }}
-          </div>
+      <div
+        v-for="duelo in fecha.duelos"
+        :key="duelo"
+        class="grid grid-cols-11 border-b"
+      >
+        <div class="text-right col-start-1 col-end-5">
+          {{ duelo.local.equipo }}
+        </div>
+        <div class="text-right col-start-5 col-end-6">
+          {{ duelo.local.puntos }}
+        </div>
+        <div class="text-center col-start-6 col-end-7">
+          vs
+        </div>
+        <div class="text-left col-start-7 col-end-8">
+          {{ duelo.visita.puntos }}
+        </div>
+        <div class="text-left col-start-8 col-end-12">
+          {{ duelo.visita.equipo }}
         </div>
       </div>
     </div>
@@ -38,16 +42,16 @@ export default {
   data () {
     return {
       fechas: [
-        { numero: 1, fecha: '13 Mayo 2021', duelos: [{ local: 'Mancoster United', visita: 'Los del Gulag' }, { local: 'The Wutang Lag', visita: 'Chichigang' }, { local: 'FPMR', visita: 'CRK' }] },
-        { numero: 2, fecha: '20 Mayo 2021', duelos: [{ local: 'Mancoster United', visita: 'Chichigang' }, { local: 'The Wutang Lag', visita: 'CRK' }, { local: 'Los del Gulag', visita: 'FPMR' }] },
-        { numero: 3, fecha: '27 Mayo 2021', duelos: [{ local: 'FPMR', visita: 'The Wutang Lag' }, { local: 'Chichigang', visita: 'Los del Gulag' }, { local: 'CRK', visita: 'Mancoster United' }] },
-        { numero: 4, fecha: '3 Junio 2021', duelos: [{ local: 'Mancoster United', visita: 'FPMR' }, { local: 'Los del Gulag', visita: 'The Wutang Lag' }, { local: 'Chichigang', visita: 'CRK' }] },
-        { numero: 5, fecha: '10 Junio 2021', duelos: [{ local: 'CRK', visita: 'Los del Gulag' }, { local: 'Chichigang', visita: 'FPMR' }, { local: 'The Wutang Lag', visita: 'Mancoster United' }] },
-        { numero: 6, fecha: '17 Junio 2021', duelos: [{ local: 'Mancoster United', visita: 'The Wutang Lag' }, { local: 'FPMR', visita: 'Chichigang' }, { local: 'Los del Gulag', visita: 'CRK' }] },
-        { numero: 7, fecha: '24 Junio 2021', duelos: [{ local: 'CRK', visita: 'Chichigang' }, { local: 'The Wutang Lag', visita: 'Los del Gulag' }, { local: 'FPMR', visita: 'Mancoster United' }] },
-        { numero: 8, fecha: '1 Julio 2021', duelos: [{ local: 'The Wutang Lag', visita: 'FPMR' }, { local: 'Los del Gulag', visita: 'Chichigang' }, { local: 'Mancoster United', visita: 'CRK' }] },
-        { numero: 9, fecha: '8 Julio 2021', duelos: [{ local: 'Chichigang', visita: 'Mancoster United' }, { local: 'CRK', visita: 'The Wutang Lag' }, { local: 'FPMR', visita: 'Los del Gulag' }] },
-        { numero: 10, fecha: '15 Julio 2021', duelos: [{ local: 'Los del Gulag', visita: 'Mancoster United' }, { local: 'Chichigang', visita: 'The Wutang Lag' }, { local: 'CRK', visita: 'FPMR' }] }
+        { numero: 1, fecha: '13 Mayo 2021', duelos: [{ local: { equipo: 'Mancoster United', puntos: '3' }, visita: { equipo: 'Los del Gulag', puntos: '3' } }, { local: { equipo: 'The Wutang Lag', puntos: '0' }, visita: { equipo: 'Chichigang', puntos: '6' } }, { local: { equipo: 'FPMR', puntos: '6' }, visita: { equipo: 'CRK', puntos: '0' } }] },
+        { numero: 2, fecha: '20 Mayo 2021', duelos: [{ local: { equipo: 'Mancoster United', puntos: '-' }, visita: { equipo: 'Chichigang', puntos: '-' } }, { local: { equipo: 'The Wutang Lag', puntos: '-' }, visita: { equipo: 'CRK', puntos: '-' } }, { local: { equipo: 'Los del Gulag', puntos: '-' }, visita: { equipo: 'FPMR', puntos: '-' } }] },
+        { numero: 3, fecha: '27 Mayo 2021', duelos: [{ local: { equipo: 'FPMR', puntos: '-' }, visita: { equipo: 'The Wutang Lag', puntos: '-' } }, { local: { equipo: 'Chichigang', puntos: '-' }, visita: { equipo: 'Los del Gulag', puntos: '-' } }, { local: { equipo: 'CRK', puntos: '-' }, visita: { equipo: 'Mancoster United', puntos: '-' } }] },
+        { numero: 4, fecha: '3 Junio 2021', duelos: [{ local: { equipo: 'Mancoster United', puntos: '-' }, visita: { equipo: 'FPMR', puntos: '-' } }, { local: { equipo: 'Los del Gulag', puntos: '-' }, visita: { equipo: 'The Wutang Lag', puntos: '-' } }, { local: { equipo: 'Chichigang', puntos: '-' }, visita: { equipo: 'CRK', puntos: '-' } }] },
+        { numero: 5, fecha: '10 Junio 2021', duelos: [{ local: { equipo: 'CRK', puntos: '-' }, visita: { equipo: 'Los del Gulag', puntos: '-' } }, { local: { equipo: 'Chichigang', puntos: '-' }, visita: { equipo: 'FPMR', puntos: '-' } }, { local: { equipo: 'The Wutang Lag', puntos: '-' }, visita: { equipo: 'Mancoster United', puntos: '-' } }] },
+        { numero: 6, fecha: '17 Junio 2021', duelos: [{ local: { equipo: 'Mancoster United', puntos: '-' }, visita: { equipo: 'The Wutang Lag', puntos: '-' } }, { local: { equipo: 'FPMR', puntos: '-' }, visita: { equipo: 'Chichigang', puntos: '-' } }, { local: { equipo: 'Los del Gulag', puntos: '-' }, visita: { equipo: 'CRK', puntos: '-' } }] },
+        { numero: 7, fecha: '24 Junio 2021', duelos: [{ local: { equipo: 'CRK', puntos: '-' }, visita: { equipo: 'Chichigang', puntos: '-' } }, { local: { equipo: 'The Wutang Lag', puntos: '-' }, visita: { equipo: 'Los del Gulag', puntos: '-' } }, { local: { equipo: 'FPMR', puntos: '-' }, visita: { equipo: 'Mancoster United', puntos: '-' } }] },
+        { numero: 8, fecha: '1 Julio 2021', duelos: [{ local: { equipo: 'The Wutang Lag', puntos: '-' }, visita: { equipo: 'FPMR', puntos: '-' } }, { local: { equipo: 'Los del Gulag', puntos: '-' }, visita: { equipo: 'Chichigang', puntos: '-' } }, { local: { equipo: 'Mancoster United', puntos: '-' }, visita: { equipo: 'CRK', puntos: '-' } }] },
+        { numero: 9, fecha: '8 Julio 2021', duelos: [{ local: { equipo: 'Chichigang', puntos: '-' }, visita: { equipo: 'Mancoster United', puntos: '-' } }, { local: { equipo: 'CRK', puntos: '-' }, visita: { equipo: 'The Wutang Lag', puntos: '-' } }, { local: { equipo: 'FPMR', puntos: '-' }, visita: { equipo: 'Los del Gulag', puntos: '-' } }] },
+        { numero: 10, fecha: '15 Julio 2021', duelos: [{ local: { equipo: 'Los del Gulag', puntos: '-' }, visita: { equipo: 'Mancoster United', puntos: '-' } }, { local: { equipo: 'Chichigang', puntos: '-' }, visita: { equipo: 'The Wutang Lag', puntos: '-' } }, { local: { equipo: 'CRK', puntos: '-' }, visita: { equipo: 'FPMR', puntos: '-' } }] }
       ]
     }
   }
