2fbf2e619eb2135feb29c7b30dd38a3dc405df38
Sergio Rodriguez <sergrodrig@gmail.com>
2021-05-20 12:35:05 -0400

    imagenes con resultados

--- src/main.js
+++ src/main.js
@@ -5,18 +5,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import './index.css'
 import LogRocket from 'logrocket'
 import { createClient } from 'villus'
 
-const client = createClient({
-  url: process.env.VUE_APP_API_URL
-})
+const client = createClient({ url: process.env.VUE_APP_API_URL })
 
 LogRocket.init('nckyti/sudoers-remuneraciones')
 
 createApp(App)
   .use(store)
-  .use(router)
-  .use(client)
+  .use(router).use(client)
   .mount('#app')
